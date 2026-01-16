c248f06ac6e0f5afa21481923f12ee228798d56a
galczo5 <kamil.galek@gmail.com>
2026-01-16 22:54:40 +0100

    Cleanup and home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,20 @@
-import type { Route } from './+types/home';
-import { Welcome } from '../welcome/welcome';
-
-export function meta({}: Route.MetaArgs) {
-  return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
-}
+import { TypographyH1, TypographyP } from '~/components/ui/typography';
+import { Button } from '~/components/ui/button';
+import { ArrowRight, Library } from 'lucide-react';
+import { Separator } from '~/components/ui/separator';
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen m-auto text-center">
+      <Library size={150} className="mb-3" />
+      <TypographyH1>Music Theory App</TypographyH1>
+      <TypographyP>Music theory training for guitarists and all musicians.</TypographyP>
+      <div className="w-96 my-6">
+        <Separator />
+      </div>
+      <Button variant="default">
+        Continue to the app <ArrowRight />
+      </Button>
+    </div>
+  );
 }
