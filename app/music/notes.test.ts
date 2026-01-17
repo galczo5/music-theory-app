51e4c7c7c66e9128618eb8f8ab555facc51be1f5
galczo5 <kamil.galek@gmail.com>
2026-01-17 11:21:36 +0100

    add vitest

---
+++ app/music/notes.test.ts
@@ -0,0 +1,16 @@
+import { expect, test } from 'vitest';
+import { interval, Note, notesArray } from '~/music/notes';
+
+test('notesArray contains 12 values', () => {
+  expect(notesArray.length).eq(12);
+});
+
+test('interval should calculate difference between lower and higher note', () => {
+  const result = interval(Note.C, Note.D);
+  expect(result).eq(2);
+});
+
+test('interval should calculate difference between higher and lower note', () => {
+  const result = interval(Note.D, Note.C);
+  expect(result).eq(2);
+});
